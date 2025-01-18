cdba394c51b7b8a0a7fb760e2b95c74f29042519
Yuchnan <ramandhanuyuchnan@gmail.com>
2025-01-19 00:27:25 +0700

    user demographics done

---
+++ admin-dashboard/src/constants/usersPage/userDemographicsData.js
@@ -0,0 +1,9 @@
+export const WARNA = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#0088FE"]
+
+export const USER_DEMOGRAPHICS_DATA = [
+    { name: "18-24", value: 20 },
+    { name: "25-34", value: 30 },
+    { name: "35-44", value: 30 },
+    { name: "45-54", value: 15 },
+    { name: "55+", value: 10 },
+]
